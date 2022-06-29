af79ada48c0a4877adef056187afb2648ef59623
sayeemuddin <msayeemuddins@systemintegration.in>
2022-06-29 17:46:03 +0530

    add react component for take measurements

--- frontend/src/components/TakeMeas.js
+++ frontend/src/components/TakeMeas.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,49 +7,50 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import "./takemeas.css";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+export function takeMeasurements(setmdata){
+
+  setmdata("hello")
+
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 function TakeMeas() {
+
+  const [mdata, setmdata]=useState("");
+  useEffect(()=>{
+
+  },[mdata]); 
+
     return (
       <>
-        <button className='takeMeasButton'>Take Measurements</button>
+        <button onClick={()=>setmdata("new h"+Math.random())} className='tm' >Take Measurements</button>
         <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Min Temp</TableCell>
+            <TableCell align="right">Max Temp</TableCell>
+            <TableCell align="right">Avg Temp</TableCell>
+            <TableCell align="right">AmbientTemp</TableCell>
+            <TableCell align="right">LightIntensity</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+         
             <TableRow
-              key={row.name}
+              key={Math.random()}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {mdata}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{mdata}</TableCell>
+              <TableCell align="right">{mdata}</TableCell>
+              <TableCell align="right">{mdata}</TableCell>
+              <TableCell align="right">{mdata}</TableCell>
             </TableRow>
-          ))}
+          
         </TableBody>
       </Table>
     </TableContainer>
